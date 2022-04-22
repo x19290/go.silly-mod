d9c5e155c25e8b6da3eece154b779726d810ce53
Hiroki Horiuchi <x19290@gmail.com>
2022-04-22 12:34:54 +0900

    fix(*): make a require line precious

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/x19290/go.silly-mod
 
 go 1.18
+
+require github.com/stretchr/testify v1.7.1
--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ rm -f go.sum
 go mod tidy
 cp go.mod go.mod-kept
 go test ./...
-sed -i '4,$d' go.mod
+sed -i '6,$d' go.mod
